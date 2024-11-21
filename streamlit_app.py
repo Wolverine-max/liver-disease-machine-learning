f81c7017374e76e351db4a37c059449f0e10ef51
Wolverine-max <himanshunutron@gmail.com>
2024-11-21 18:19:06 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.title('Liver prediction App')
 
-st.info('This is app for Liver Prediction on the basis of Medical Data! ')
+st.info('This app is for Liver Prediction on the basis of Medical Data! ')
