15225fc773e6a02c107a8fa0fc7db0f7dc0e2cd1
Wolverine-max <himanshunutron@gmail.com>
2024-11-21 18:29:54 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,3 +3,5 @@ import streamlit as st
 st.title('Liver prediction App')
 
 st.info('This app is for Liver Prediction on the basis of Medical Data! ')
+df=pd.read_csv('https://github.com/Wolverine-max/liver-disease-machine-learning/blob/master/indian_liver_patient.csv')
+df
